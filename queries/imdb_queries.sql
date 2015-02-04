5ca34cfda388e6b177fecf860237eae66c06c124
Patrick Skjennum <mail@habitats.no>
2015-02-04 14:54:01 +0100

    added sql query file

---
+++ queries/imdb_queries.sql
@@ -0,0 +1,6 @@
+##############################################
+# Handy SQL queries for the original imdb db #
+##############################################
+
+# select movie info table
+select * from movie_info
