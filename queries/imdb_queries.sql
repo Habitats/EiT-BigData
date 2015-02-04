2c09c5c0c0811e39c93e25f92abcaa1e9f22801c
Martin Ringdal <mringdal@gmail.com>
2015-02-04 14:59:57 +0100

    Update imdb_queries.sql
    Query for actors and rating

--- queries/imdb_queries.sql
+++ queries/imdb_queries.sql
@@ -4,3 +4,17 @@
 
 # select movie info table
 select * from movie_info
+
+# Select movie id, title, rating and actors
+SELECT t.id, title, info AS rating, p.name
+FROM title AS t
+INNER JOIN movie_info_idx AS i
+ON t.id = i.movie_id
+INNER JOIN cast_info AS ci
+ON ci.movie_id = t.id
+INNER JOIN name AS p
+ON p.id = ci.person_id
+WHERE t.kind_id = 1 # Movies
+AND i.info_type_id = 101
+AND ci.person_role_id = 1 # Actor
+LIMIT 100
