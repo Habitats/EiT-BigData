2c7068f783c4664aec273baabdd7a18ad2761fec
Kjartan Kloster Osmundsen <kjartan.osmundsen@gmail.com>
2015-02-11 13:57:27 +0100

    Div. nøkkeldata

--- queries/imdb_queries.sql
+++ queries/imdb_queries.sql
@@ -29,3 +29,29 @@ JOIN info_type ty ON m.info_type_id = ty.id
 WHERE t.title ='Inception'
 AND ty.info = 'gross'
 AND m.info LIKE '%Worldwide%'
+
+
+# Tittel, rating, votes, sjanger, språk, lanseringsdato, profit, plot
+# Returnerer bare første sjanger når filmen har flere sjangere.
+# Dette gjelder også språk, lanseringsdato, profit og plot.
+
+SELECT t.id, t.title, t.production_year,
+(SELECT info FROM movie_info_idx WHERE info_type_id = 101 AND movie_id = t.id) AS rating,
+
+(SELECT info FROM movie_info_idx WHERE info_type_id = 100 AND movie_id = t.id) AS votes,
+
+(SELECT info FROM movie_info WHERE info_type_id = 3 AND movie_id = t.id LIMIT 1) AS sjanger,
+
+(SELECT info FROM movie_info WHERE info_type_id = 4 AND movie_id = t.id LIMIT 1) AS språk,
+
+(SELECT info FROM movie_info WHERE info_type_id = 16 AND movie_id = t.id LIMIT 1) AS lanseringsdato,
+
+(SELECT info FROM movie_info WHERE info_type_id = 107 AND movie_id = t.id LIMIT 1) AS profit,
+
+(SELECT info FROM movie_info WHERE info_type_id = 98 AND movie_id = t.id LIMIT 1) AS plot
+
+FROM title t
+
+WHERE t.kind_id = 1 # Only movies
+
+AND EXISTS (SELECT info FROM movie_info_idx WHERE info_type_id = 101 AND movie_id = t.id) # IMDB-rating exists
