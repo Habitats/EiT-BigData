4e4498cf4a1ea03731172e5887653b21b82f450b
Patrick Skjennum <mail@habitats.no>
2015-02-18 13:12:01 +0100

    gitignore and queries

--- queries/imdb_queries.sql
+++ queries/imdb_queries.sql
@@ -2,7 +2,7 @@
 # Handy SQL queries for the original imdb db #
 ##############################################
 
-# select movie info table
+# Select movie info table
 select * from movie_info
 
 # Select movie id, title, rating and actors
@@ -19,17 +19,17 @@ AND i.info_type_id = 101
 AND ci.person_role_id = 1 # Actor
 LIMIT 100
 
-
-#Worldwide gross
-SELECT title, cast(replace( substring(substring_index(m.info,' ',1),2), ",","") as unsigned) as yo, m.info
+# Worldwide gross
+SELECT title, max(cast(replace( substring(substring_index(m.info,' ',1),2), ",","") as unsigned)) as gross
 FROM title t
 JOIN movie_info m ON m.movie_id = t.id
 JOIN info_type ty ON m.info_type_id = ty.id
 AND ty.info = 'gross'
-AND m.info LIKE '$%Worldwide)' 
-order by yo desc
+group by title
+order by gross desc
+limit 1000
 
-# Alle produsert etter Ã¥r 2000 med minst 1000 stemmer
+# Alle produsert etter år 2000 med minst 1000 stemmer
 SELECT title, votes.info as votes, rating.info as rating, money.info as money FROM title
 INNER JOIN movie_info_idx as votes
 ON title.id = votes.movie_id
@@ -44,11 +44,9 @@ AND rating.info_type_id = 101 # rating
 AND money.info_type_id = 107 # money
 AND votes.info > 1000
 
-
 # Tittel, rating, votes, sjanger, språk, lanseringsdato, profit, plot
 # Returnerer bare første sjanger når filmen har flere sjangere.
 # Dette gjelder også språk, lanseringsdato, profit og plot.
-
 SELECT t.id, t.title, t.production_year,
 (SELECT info FROM movie_info_idx WHERE info_type_id = 101 AND movie_id = t.id) AS rating,
 (SELECT info FROM movie_info_idx WHERE info_type_id = 100 AND movie_id = t.id) AS votes,
@@ -61,7 +59,6 @@ FROM title t
 WHERE t.kind_id = 1 # Only movies
 AND EXISTS (SELECT info FROM movie_info_idx WHERE info_type_id = 101 AND movie_id = t.id) # IMDB-rating exists
 
-
 # Legge TOP 1000 ACTORS inn i mysql
 CREATE TABLE top1000actors (
   position INT NOT NULL AUTO_INCREMENT,
@@ -70,13 +67,9 @@ CREATE TABLE top1000actors (
   PRIMARY KEY (position)
 );
 
-
-
 LOAD DATA LOCAL INFILE 'c:/top1000actors.csv'
-
 INTO TABLE top1000actors
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY ''
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
-
